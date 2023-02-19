5f72f8334c4c491ea9643910ff60308ee7a4cec4
jwoohaha <jwoohaha@gmail.com>
2023-02-19 22:59:53 +0900

    Time: 23 ms (97.31%), Space: 14.1 MB (14.93%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,31 @@
+class MyQueue:
+    # stack 2개를 사용하여 queue 구현
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        # output이 없으면 input stack에서 output stack으로 입력
+        # 한 번 더 스택을 거쳐 LIFO에서 FIFO으로 변환
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        return self.input == [] and self.output == []
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
